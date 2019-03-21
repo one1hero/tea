37e16be467b7deed4df5c3e2254a70e6b8d02e69
liubin <liubin.b@qq.com>
2019-03-21 17:24:29 +0800

    添加订单删除

--- teaServer/src/main/java/com/newer/tea/controller/HomeController.java
+++ teaServer/src/main/java/com/newer/tea/controller/HomeController.java
@@ -1,15 +1,22 @@
 package com.newer.tea.controller;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.newer.tea.From;
+import com.newer.tea.Order;
 import com.newer.tea.service.FromService;
 import com.newer.tea.service.OrderService;
 
 @RestController
+@RequestMapping("/tea/v1")
 @CrossOrigin
 public class HomeController {
 	
@@ -22,6 +29,14 @@ public class HomeController {
 	@GetMapping("/")
 	public From home(){
 		return fromService.orderFrom();
+	}
+	
+	@PostMapping("/add")
+	public HashMap<String, String> addOrder(@RequestBody Order order){
+		HashMap<String, String> map = new HashMap<>();
+		orderService.subOrder(order);
+		
+		return null;
 	}
 
 }
--- teaServer/src/main/java/com/newer/tea/service/OrderService.java
+++ teaServer/src/main/java/com/newer/tea/service/OrderService.java
@@ -3,6 +3,7 @@ package com.newer.tea.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.newer.tea.Order;
 import com.newer.tea.repository.OrderRepository;
 
 @Service
@@ -10,6 +11,10 @@ public class OrderService {
 	
 	@Autowired
 	OrderRepository mongo;
+	
+	public void subOrder(Order order) {
+		mongo.insert(order);
+	}
 	
 	
 
